f4772c403b556c7c737d7f3cad2b059570ef58e2
Henry <9332671@qq.com>
2017-03-25 15:48:28 +0800

    jquery

--- easy.js
+++ easy.js
@@ -1,3 +1,17 @@
 (function(window,undefined){
+    var document = window.document;
+    var _Easy = function(selector,context){
+        return new _Easy.fn.init(selector,context);
+    }
 
+    _Easy.fn = _Easy.prototype = {
+        version:'1.0',
+        constructor:_Easy,
+        init:function(selector,context){
+
+        }
+
+    }
+
+    _Easy.fn.init.prototype = _Easy.fn;
 })(window,undefined)
